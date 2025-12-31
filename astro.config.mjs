cd4cb563f73d69f23a4f9ffa0f5ac6c56dddc010
daives01 <danielives012@yahoo.com>
2025-12-31 10:41:31 -0700

    feat: improve SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,15 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
+import sitemap from '@astrojs/sitemap';
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  // site: 'https://your-domain.com',
+  site: 'https://qbuilderscolorado.com',
   vite: {
     plugins: [tailwindcss()]
   },
-
+  integrations: [sitemap()],
   adapter: cloudflare()
 });
